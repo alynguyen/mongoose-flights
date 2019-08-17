9fbfcb2e50ba789aafb7a4ca13a5f89d1283cc2e
Aly Nguyen <alynguyen@KDIMOU.localdomain>
2019-08-17 16:28:07 -0700

    reference flight schema in tickets

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,7 +3,7 @@ var Flight = require('../models/flight');
 
 module.exports = {
   newTicket,
-  addTicket
+  addTicket,
 };
 
 function newTicket(req, res) {
@@ -15,4 +15,32 @@ function newTicket(req, res) {
   });
 }
 
+function addTicket(req, res) {
+  let ticket = new Ticket(req.body);
+  // ticket.flight.push(req.body.flight);
+  console.log(ticket, req.body.flight);
+  ticket.save(function(err) {
+    if (err) return res.render('flights/:id/tickets/new');
+      res.redirect(`/flights/${ticket.flight}`);
+  });
+}
 
+
+// function show(req, res) {
+//   Flight.findById(req.params.id, function (err, flight) {
+//     res.render('flights/show', {title: 'Flight Detail', flight});
+//     console.log('ran');
+//   });
+// }
+
+
+// function create(req, res) {
+//   for (let key in req.body) {
+//     if (req.body[key] === '') delete req.body[key];
+//   }
+//   let flight = new Flight(req.body);
+//   flight.save(function(err) {
+//     if (err) return res.render('flights/new');
+//     res.redirect('/flights');
+//   });
+// }
--- routes/tickets.js
+++ routes/tickets.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var ticketsController = require('../controllers/tickets');
 
 router.get('/flights/:id/tickets/new', ticketsController.newTicket);
+router.post('/flights/:id/tickets/new', ticketsController.addTicket);
 
 module.exports = router;
